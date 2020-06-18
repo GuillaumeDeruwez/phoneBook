612cc168e31e7627a84b6525d1133ab56c9c87e7
Deruwez Guillaume <gderuwez@gmail.com>
2020-06-18 05:34:09 +0200

    Updates

---
+++ precache-manifest.fc4445d8edd0df84a477100e50c52d70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b894981a88d338f5e3ce74398a9954a",
+    "url": "/phoneBook/index.html"
+  },
+  {
+    "revision": "8b5a4306fbc143c79f22",
+    "url": "/phoneBook/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "34ef7dd54f610099a659",
+    "url": "/phoneBook/static/js/2.f6b04295.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/phoneBook/static/js/2.f6b04295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b5a4306fbc143c79f22",
+    "url": "/phoneBook/static/js/main.477f4b2b.chunk.js"
+  },
+  {
+    "revision": "895907593da1bef614cd",
+    "url": "/phoneBook/static/js/runtime-main.6d317dd7.js"
+  }
+]);
